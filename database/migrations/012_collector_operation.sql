47590b9a496db4216b492eec872cb14cafd12784
Martijn Kerssing <martijn@kerssing.nl>
2026-08-31 21:53:46 +0200

    Add collector operational commands

---
+++ database/migrations/012_collector_operation.sql
@@ -0,0 +1,11 @@
+ALTER TABLE output_commands
+    DROP CONSTRAINT IF EXISTS output_commands_supported_output;
+
+ALTER TABLE output_commands
+    ADD CONSTRAINT output_commands_supported_output
+    CHECK (output_id IN (
+        'R1','R2','R3','R4','R5','R6','R7','R8',
+        'STOP','FILTERPUMP_ON','FILTERPUMP_OFF',
+        'HEATPUMP_ON','HEATPUMP_OFF',
+        'COLLECTOR_OPEN','COLLECTOR_CLOSE'
+    ));
