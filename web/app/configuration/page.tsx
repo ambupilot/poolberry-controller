43daa1aa55d626b8d8042c469158217d466f7f01
Martijn Kerssing <martijn@kerssing.nl>
2026-08-31 19:27:33 +0200

    feat: enable hardware navigation from configuration

--- web/app/configuration/page.tsx
+++ web/app/configuration/page.tsx
@@ -64,7 +64,7 @@ export default async function ConfigurationPage() {
         <Link className="tab" href="/">Dashboard</Link>
         <Link className="tab active" href="/configuration">Configuratie</Link>
         <span className="tab disabled">Programma&apos;s</span>
-        <span className="tab disabled">Hardware</span>
+        <Link className="tab" href="/hardware">Hardware</Link>
         <span className="tab disabled">Historie</span>
         <span className="tab disabled">Events</span>
         <span className="tab disabled">Systeem</span>
