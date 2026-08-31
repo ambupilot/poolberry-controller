3272a41ccb31b0ebe6565f013c469cd92082cd9d
Martijn Kerssing <martijn@kerssing.nl>
2026-08-31 20:42:45 +0200

    Allow STOP command

---
+++ database/migrations/009_stop_command.sql
@@ -0,0 +1,6 @@
+ALTER TABLE output_commands
+    DROP CONSTRAINT IF EXISTS output_commands_supported_output;
+
+ALTER TABLE output_commands
+    ADD CONSTRAINT output_commands_supported_output
+    CHECK (output_id IN ('R1','R2','R3','R4','R5','R6','R7','R8','STOP'));
